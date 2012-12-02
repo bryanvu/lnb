0c2111bae5e191ed89cf4407ed61817d0c7385a8
Bryan Vu <bryanv@gmail.com>
2012-12-02 14:47:37 -0800

    initial website created

---
+++ app/controllers/wedding_controller.rb
@@ -0,0 +1,4 @@
+class WeddingController < ApplicationController
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lnb::Application.config.secret_token = '628624265274e5ec5df440504b97499ac21630b0c1387882917234c8ba7edb68e73af08d745f6b8a43169d93ed7d588f665e14a972664d56b555e4259c7d6145'
